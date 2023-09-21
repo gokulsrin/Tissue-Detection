6c288b5c07d8493c4c69accdc35f0c0d44e824bd
gokulsrin <gokul.srinivasan.23@dartmouth.edu>
2023-09-20 23:22:40 -0400

    fixed

--- Tissue_Detection/.ipynb_checkpoints/Tissue_Detection-checkpoint.py
+++ Tissue_Detection/.ipynb_checkpoints/Tissue_Detection-checkpoint.py
@@ -5,34 +5,74 @@ import slideio
 import numpy as np 
 import cv2
 import matplotlib.pyplot as plt
-import shapely 
+import shapely
+from shapely import Polygon, MultiPolygon
+from shapely.affinity import translate 
+from shapely.affinity import scale 
 from matplotlib.patches import Polygon as MPolygon
 from matplotlib.collections import PatchCollection
 
+
 class TissueDetector():
+    """
+    This class is meant to help you generate a shapely polygon that contains the tissue regions in a WSI. 
+    
+    In particular, this class can be very useful when generating patch data for histopathology CV algorithms, where we want 
+    to only generate data for those patches that are within the tissue and not in the whitespace. The SAM model with a box prompt is used to
+    produce and collect these contours. 
+    
+    Methods:
+    --------
+    init() -> None
+        This will just init the TissueDetector class. Pass to it the relevant device. 
+        
+    find_tissue_contours(np_im, erosion_val = 0.05) -> None
+        This method finds the tissue regions within the WSI. First the WSI is resized down so the smallest axis is 1024 pixels in length. 
+        More efficient computation is performed on this scaled down image. Then the generated contour is scaled back up. The erosion_val dicates how much smaller the box prompt
+        is relative to the WSI. So the larger the erosion_val, the smaller the box prompt. 
+    
+    get_contours() -> Shapely Polygon/MultiPolygon
+        This method returns the Polygon/MultiPolygon that describes the tissue regions. 
+    
+    plot_annotations() -> None
+        Visualizes the generated segmentations on the WSI. Note that this method will also visualize small artifacts that are not returned as Polygons. 
+    
+    Examples:
+    ---------
+    >>> device = torch.device('cuda:3' if torch.cuda.is_available() else 'cpu')
+    >>> detector = TissueDetector(device) 
+    >>> np_im = np.load("/dartfs/rc/nosnapshots/V/VaickusL-nb/EDIT_Students/projects/spatial_omics/DH/held_out_wsi/8_A1_0_orig.npy")
+    >>> detector.find_tissue_contours(np_im)
+    >>> detector.plot_annotations()
+    
+    Image of the contours plotted on top of the WSI 
+    
+    >>> polygon = detector.get_contours()
+        
+    Shapely Polygon
+    
+    Notes:
+    ------
+    Please note that you might need to change the erosion_val to get better results depending on your task. 
+    
+    """
+    
     def __init__(self, device): #init all the model stuff 
         self.DEVICE = device
-        self.CHECKPOINT_PATH = "/dartfs/rc/nosnapshots/V/VaickusL-nb/EDIT_Students/users/Gokul_Srinivasan/Tissue_Detection/Tissue_Detection/sam_vit_h_4b8939.pth"
+        self.CHECKPOINT_PATH = "/dartfs/rc/nosnapshots/V/VaickusL-nb/EDIT_Students/users/Gokul_Srinivasan/Tissue_Detection/sam_vit_h_4b8939.pth"
         self.MODEL_TYPE = "vit_h"
         
         self.sam = sam_model_registry[self.MODEL_TYPE](checkpoint=self.CHECKPOINT_PATH).to(device=self.DEVICE)
-        self.mask_generator = SamAutomaticMaskGenerator(
-            model=self.sam,
-            points_per_side=32,
-            points_per_batch=64, 
-            pred_iou_thresh=0.86,
-            stability_score_thresh=0.92,
-            min_mask_region_area=int(1e5), 
-        )
+    
         self.reduced_img = None
         self.scale_ratio = None
         self.contours = []
-        self.scaled_contours = []
+        self.small_contours = []
         
-    def find_tissue_contours(self, np_im):
+    def find_tissue_contours(self, np_im, erosion_val=0.05):
         #reset after every call
         self.contours = []
-        self.scaled_contours = []
+        self.small_contours = []
         
         min_dim_size = 1028
         min_dim = min(np_im.shape[1], np_im.shape[0])
@@ -41,21 +81,37 @@ class TissueDetector():
         res = cv2.resize(np_im, dsize=(int(self.scale_ratio*np_im.shape[1]), int(self.scale_ratio*np_im.shape[0])), interpolation=cv2.INTER_CUBIC)
         self.reduced_img = res 
         
-        sam_result = self.mask_generator.generate(res)
+        input_box = np.array([int(0+erosion_val*res.shape[1]), int(0+erosion_val*res.shape[0]), int(res.shape[1]-erosion_val*res.shape[1]), int(res.shape[0] - erosion_val*res.shape[0])])
         
-        big_regions = []
-        for r in sam_result:
-            if r["area"] > 5e4: 
-                big_regions.append(r)
+        predictor = SamPredictor(self.sam)
+        predictor.set_image(res)
+
+        masks, _, _ = predictor.predict(
+            point_coords=None,
+            point_labels=None,
+            box=input_box[None, :],
+            multimask_output=False,
+        )
+        masks = masks.squeeze()
+
+        binary_mask = (masks * 255).astype(np.uint8)
+        small_contours, _ = cv2.findContours(binary_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+        #now extract the polygon and scale up 
+        minimum_thresh = 1e4
+        polygons = []
+        for i in small_contours: 
+            contour_coords = i.squeeze()
+            if contour_coords.ndim == 1: 
+                continue  
+            poly = Polygon(contour_coords)
+            if poly.buffer(0).area > minimum_thresh: 
+                polygons.append(poly.buffer(0))
                 
-        big_regions = sorted(big_regions, key=lambda x: x["area"], reverse=False)
-        big_regions = big_regions[:-1] #include everything but the big background segmentation
-        
-        for region in big_regions:
-            binary_mask = (region["segmentation"] * 255).astype(np.uint8)
-            contour, _ = cv2.findContours(binary_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            self.contours.append(contour[0]*(int(1/self.scale_ratio)))
-            self.scaled_contours.append(contour[0])
+        multi_polygon = MultiPolygon(polygons)
+        multi_polygon = scale(multi_polygon, yfact = int(1/self.scale_ratio), xfact = int(1/self.scale_ratio))
+        self.contours = multi_polygon
+        self.small_contours = small_contours
                          
     def get_contours(self):
         return self.contours 
@@ -67,7 +123,7 @@ class TissueDetector():
         ax.imshow(self.reduced_img)
 
         # For each contour, create a polygon and add it to the plot
-        for contour in self.scaled_contours:
+        for contour in self.small_contours:
             # Contour is an array of shape (N, 1, 2). We reshape it to (N, 2)
             reshaped_contour = contour.reshape(-1, 2)
             # Create a Polygon object
@@ -76,4 +132,5 @@ class TissueDetector():
             ax.add_patch(polygon)
 
         plt.show()
+
         
